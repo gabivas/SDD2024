9a8f0d285ddb20d9926d957021b0c957fab4c492
gabriel.vasilescu <gabriel.vasilescu@eos-it.ro>
2024-05-21 19:26:17 +0300

    Seminar 13

--- Seminar13_Grafuri.c
+++ Seminar13_Grafuri.c
@@ -134,6 +134,61 @@ void afisareAutostrada(Autostrada a) {
 	printf("\nAutostrada %s cu id-ul %d are %d km", a.denumire, a.id, a.nrKm);
 }
 
+NodPrincipal* cautareNodDupaId(NodPrincipal* graf, int id) {
+	while (graf !=NULL && id != graf->info.id) {
+		graf = graf->next;
+	}
+	return graf;
+}
+
+void inserareMuchie(NodPrincipal* graf, int id1, int id2) {
+	NodPrincipal* nod1 = cautareNodDupaId(graf, id1);
+	NodPrincipal* nod2 = cautareNodDupaId(graf, id2);
+	if (nod1 != NULL && nod2 != NULL) {
+		inserareListaSecundara(&nod1->vecini, nod2);
+		inserareListaSecundara(&nod2->vecini, nod1);
+	}
+}
+
+void afisareGraf(NodPrincipal* graf) {
+	while (graf != NULL) {
+		afisareAutostrada(graf->info);
+		NodSecundar* vecini = graf->vecini;
+		printf("\n Vecinii:");
+		while (vecini) {
+			afisareAutostrada(vecini->info->info);
+			vecini = vecini->next;
+		}
+		printf("\n");
+		graf = graf->next;
+	}
+}
+
+void parcurgereInAdancime(NodPrincipal* graf, int idStart) {
+	int dimensiune = 6;
+	int* vectorNoduriVizitate = (int*)malloc(6 * sizeof(int));
+	for (int i = 0; i < dimensiune; i++) {
+		vectorNoduriVizitate[i] = 0;
+	}
+	ListaDubla stiva;
+	stiva.first = stiva.last = NULL;
+	push(&stiva, idStart);
+	vectorNoduriVizitate[idStart-1] = 1;
+	while (stiva.first) {
+		int idExtras = pop(&stiva);
+		NodPrincipal* nodExtras = cautareNodDupaId(graf, idExtras);
+		afisareAutostrada(nodExtras->info);
+		NodSecundar* vecini = nodExtras->vecini;
+		while (vecini) {
+			if (vectorNoduriVizitate[vecini->info->info.id - 1] == 0) {
+				push(&stiva, vecini->info->info.id);
+				vectorNoduriVizitate[vecini->info->info.id - 1] = 1;
+			}
+			vecini = vecini->next;
+		}
+	}
+}
+
 void main() {
 	NodPrincipal* graf = NULL;
 	inserareListaPrincipala(&graf, initAutostrada(1, "A1", 100));
@@ -143,4 +198,14 @@ void main() {
 	inserareListaPrincipala(&graf, initAutostrada(5, "A5", 500));
 	inserareListaPrincipala(&graf, initAutostrada(6, "A6", 600));
 
+	inserareMuchie(graf, 1, 2);
+	inserareMuchie(graf, 1, 3);
+	inserareMuchie(graf, 2, 4);
+	inserareMuchie(graf, 3, 5);
+	inserareMuchie(graf, 4, 6);
+	inserareMuchie(graf, 5, 6);
+
+	afisareGraf(graf);
+	printf("\nParcurgere in adancime:");
+	parcurgereInAdancime(graf, 1);
 }
